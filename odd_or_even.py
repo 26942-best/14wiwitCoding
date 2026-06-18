2c3c919fa8b77d3ee7ccf0541a5a551160a9d674
26942-best <26942@bbw.ac.th>
2026-06-18 13:03:04 +0700

    Even Or Odd program
    งานวัน 15/6/69

---
+++ odd_or_even.py
@@ -0,0 +1,12 @@
+print("Program name : Even or Odd")
+
+Number = int(input("Enter your number : "))
+
+if Number %2 == 0 :
+    print("Even Numbers")
+else:
+    print("Odd Numbers")
+
+print("By best 4/4")
+
+print("Thank you")
