208d2ef2d59572f974fe7e3156c5b28df9cde927
26942-best <26942@bbw.ac.th>
2026-07-20 08:46:19 +0700

    14 best multiplier ข้อ 3
    9/7/69

---
+++ 14mutiplier3.py
@@ -0,0 +1,13 @@
+print("x", end="\t")
+for col in range(1, 13):
+    print(col, end="\t")
+print()
+ 
+for row in range(1, 13):
+    print(row, end="\t")
+    for col in range(1, 13):
+        result = row * col
+        print(result, end="\t")
+    print()
+
+print("โปรแกรมหาแม่สูตรคูณ by best 14")
